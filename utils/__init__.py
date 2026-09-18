d048386573f6ac129978f91ae6eaa59ac78da58d
zhangst <shuntongzhang@gmail.com>
2026-09-18 20:34:38 +0800

    Remove unused labs, datasets, and utilities

--- utils/__init__.py
+++
@@ -1,3 +0,0 @@
-from .utils import create_index, create_search_index, check_index_ready, set_env, get_llm
-
-__all__ = ["create_index", "create_search_index", "check_index_ready","set_env", "get_llm"]
